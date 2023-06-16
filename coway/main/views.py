7986ceca54042cf02dc725e0caf11126a8c7a9eb
mk1329 <mk1329@dgu.ac.kr>
2023-06-16 16:28:56 +0900

    Feat: 링크연결

--- coway/main/views.py
+++ coway/main/views.py
@@ -12,3 +12,39 @@ def convinfopage(request):
 
 def convmappage(request):
     return render(request, 'main/conv/conv_map.html')
+
+def joinpage(request):
+    return render(request, 'main/login/join.html')
+
+def loginpage(request):
+    return render(request, 'main/login/login.html')
+
+def askpage(request):
+    return render(request, 'main/myhome/ask.html')
+
+def editpage(request):
+    return render(request, 'main/myhome/edit.html')
+
+def homepage(request):
+    return render(request, 'main/myhome/home.html')
+
+def stpcafepage(request):
+    return render(request, 'main/stamp/stamp_cafe.html')
+
+def stpconvpage(request):
+    return render(request, 'main/stamp/stamp_conv.html')
+
+def stprestpage(request):
+    return render(request, 'main/stamp/stamp_rest.html')
+
+def stpstdpage(request):
+    return render(request, 'main/stamp/stamp_std.html')
+
+def easyroadpage(request):
+    return render(request, 'main/road/easy_road.html')
+
+def roadchoicepage(request):
+    return render(request, 'main/road/road_choice.html')
+
+def shortroadpage(request):
+    return render(request, 'main/road/short_road.html')
